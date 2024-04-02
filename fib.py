a8bdc0ff5a9533d8a957842ebc64f02f97ca3eb6
Ganesh Kalyanaraman <ganesh@gk-macbook-pro.lan>
2024-04-02 01:01:02 -0400

    initial version

---
+++ fib.py
@@ -0,0 +1,5 @@
+#generate fibonacci series upto 10
+a,b=0,1
+for i in range(10):
+    print(a)
+    a,b=b,a+b
---
+++ list.py
@@ -0,0 +1,5 @@
+#print 5 names
+names = ['John', 'Paul', 'George', 'Ringo', 'Pete'] #list of names
+for name in names:
+    print(name) #print each name
+# The list of names is stored in the variable names. The for loop iterates over each name in the list and prints it.    
